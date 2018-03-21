b41893f9fc1f57cb0248b23a0355ae98c477a32a
Arthur WAJSBROT <awajsbro@e2r7p3.42.fr>
2018-03-21 12:13:39 +0100

    reset

--- ft_uitoab.c
+++ ft_uitoab.c
@@ -6,16 +6,16 @@
 /*   By: awajsbro <awajsbro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/27 17:43:09 by awajsbro          #+#    #+#             */
-/*   Updated: 2018/03/12 16:48:39 by awajsbro         ###   ########.fr       */
+/*   Updated: 2018/03/16 12:24:52 by awajsbro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#define FT_BASE_CHARSET "0123456789abcdefghijklmnopqrstuvwxz"
 
 char	*ft_uitoab(unsigned long long n, char b, char *out)
 {
 	char		cnt;
-	static char	*base = "0123456789abcdef";
 
 	cnt = ft_ucntb(n, b);
 	if (out == NULL || b < 2 || b > 16)
@@ -23,9 +23,9 @@ char	*ft_uitoab(unsigned long long n, char b, char *out)
 	out[cnt + 0] = 0;
 	while (--cnt)
 	{
-		out[cnt + 0] = base[n % b];
+		out[cnt + 0] = FT_BASE_CHARSET[n % b];
 		n = n / b;
 	}
-	out[cnt + 0] = base[n % b];
+	out[cnt + 0] = FT_BASE_CHARSET[n % b];
 	return (out);
 }
